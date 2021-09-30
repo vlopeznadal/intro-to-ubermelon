bf7dab8f10c67c124e7cfbf1799d0cc6cdce090e
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-30 07:59:27 -0500

    Comments added explaining lines of code

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") # opening file's information to use in program
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): # function to print specific lines in text file
+    for line in log_file: # for each line in the text file, do something
+        line = line.rstrip() # take the line of text and remove white space
+        day = line[0:3] # looking at the first three characters of the string
+        if day == "Tue": # if the first three characters of the string are "Tue"...
+            print(line) #... print the line 
 
 
-sales_reports(log_file)
+sales_reports(log_file) # call the function and pass a text file for it to use
